94c0f2b78539c6505d7c85d49a367bc21d6b7135
svatos331 <54244300+svatos331@users.noreply.github.com>
2021-04-14 23:16:35 +0300

    Map || some
    Map || some function

---
+++ mapFunction.js
@@ -0,0 +1,8 @@
+const arrayForMap = [2, 3, 7, 9, 1, 4, 5, 8];
+const mapFunction = (arr, fn) => {
+    let finishArray = []
+    for (const element of arr) {
+        finishArray.push(fn(element)) 
+    }
+    return finishArray;
+};
--- someFunction.js
+++ someFunction.js
@@ -7,3 +7,18 @@ const some = (arr, fn) => {
 };
 console.log(some(arrayForSome, (el) => el === "one")) // true
 console.log(some(arrayForSome, (el) => el === "one2")) // false
+
+
+
+
+
+// map
+const arrayForMap = [2, 3, 7, 9, 1, 4, 5, 8];
+
+const some = (arr, fn) => {
+    let finishArray = []
+    for (const element of arr) {
+        (fn(element))? finishArray.push(element) : finishArray.push(null) 
+    }
+    return false;
+};
