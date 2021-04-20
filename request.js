74abee4dd1b2ac9efdb9d1768a918b6fcf7641de
svatos331 <54244300+svatos331@users.noreply.github.com>
2021-04-20 21:17:48 +0300

    request for js
    request for js

---
+++ request.js
@@ -0,0 +1,19 @@
+
+async function fetchAsinc(){ // it`s Promise
+    console.log(`starts request`)
+
+    try{
+        const response = await fetch("URL")
+        const data = await response.json()
+        console.log(`data: ${data}`)
+    } 
+
+    catch(err) {
+        console.log(`err: ${err}`)
+    } 
+
+    finally {
+
+    }
+
+}
